ab2412990eaf4e45c6908c2478bcc6a9811e6917
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-03-07 22:09:49 +0300

    Main routing component file and its css is updated

--- src/NotebookApp.jsx
+++ src/NotebookApp.jsx
@@ -6,20 +6,30 @@ import UpdateNotePage from './pages/UpdateNotePage';
 import AddNotePage from './pages/AddNotePage';
 import ErrorPage from './pages/ErrorPage';
 import Header from './components/Header';
+import AuthProvider from './context/AuthProvider';
+import LoginPage from './pages/LoginPage';
+import LogoutPage from './pages/LogoutPage';
+import RegisterPage from './pages/RegisterPage';
 
 const NotebookApp = () => {
 	return (
 		<div className="notebookApp">
-			<Router>
-				<Header />
-				<Routes>
-					<Route path="/home" element={<HomePage />} />
-					<Route path="/notes" element={<ListNotesPage />} />
-					<Route path="/update-note/:id" element={<UpdateNotePage />} />
-					<Route path="/add-note" element={<AddNotePage />} />
-					<Route path="*" element={<ErrorPage />} />
-				</Routes>
-			</Router>
+			<AuthProvider>
+				<Router>
+					<Header />
+					<Routes>
+						<Route path="/" element={<LoginPage />} />
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/register" element={<RegisterPage />} />
+						<Route path="/home" element={<HomePage />} />
+						<Route path="/notes" element={<ListNotesPage />} />
+						<Route path="/update-note/:id" element={<UpdateNotePage />} />
+						<Route path="/add-note" element={<AddNotePage />} />
+						<Route path="/logout" element={<LogoutPage />} />
+						<Route path="*" element={<ErrorPage />} />
+					</Routes>
+				</Router>
+			</AuthProvider>
 		</div>
 	);
 };

99fffa84244e885ccf61b9cf85567991cb6d8e89
Ahmet Sami Yeniçeri <ahmet.sami.yeniceri@ericsson.com>
2024-03-07 22:08:46 +0300

    api endpoints are updated

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -5,21 +5,29 @@ export const apiClient = axios.create({
 });
 
 export const retrieveAllNotesOfUser = (username) => {
-	return apiClient.get(`/api/notebook/${username}/notes`);
+	return apiClient.get(`/api/${username}/notes`);
 };
 
 export const retrieveSingleNoteOfUser = (username, id) => {
-	return apiClient.get(`/api/notebook/${username}/notes/${id}`);
+	return apiClient.get(`/api/${username}/notes/${id}`);
 };
 
-export const addNote = (username, todo) => {
-	return apiClient.post(`/api/notebook/${username}/notes`, todo);
+export const addNote = (username, note) => {
+	return apiClient.post(`/api/${username}/notes`, note);
 };
 
-export const updateNote = (username, id, todo) => {
-	return apiClient.put(`/api/notebook/${username}/notes/${id}`, todo);
+export const updateNote = (username, id, note) => {
+	return apiClient.put(`/api/${username}/notes/${id}`, note);
 };
 
 export const deleteNoteOfUser = (username, id) => {
-	return apiClient.delete(`/api/notebook/${username}/notes/${id}`);
+	return apiClient.delete(`/api/${username}/notes/${id}`);
+};
+
+export const registerUser = (user) => {
+	return apiClient.post('/api/register', user);
+};
+
+export const retrieveRegisteredUser = () => {
+	return apiClient.get(`/api/users`);
 };
